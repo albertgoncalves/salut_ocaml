e3b95ba98b4cea0c96b6d6bd0ea7ae61eef32588
albertgoncalves <goncalves.albert@gmail.com>
2018-11-22 00:42:35 -0500

    Tail rec.

--- fib.ml
+++ fib.ml
@@ -1,5 +1,11 @@
-let rec fib n =
-    if n < 2 then 1 else fib (n-1) + fib (n-2);;
+(* https://github.com/dhammika-marasinghe/Learn-OCaml/tree/master/lab03%20Tail%20Recursive%20Functions *)
+
+let fib n =
+    let rec loop i a b =
+        if i = n then a
+        else loop (i+1) (b) (a+b) in
+    loop 0 0 1;;
+
 let main () =
     let arg = int_of_string Sys.argv.(1) in
     print_int (fib arg);
