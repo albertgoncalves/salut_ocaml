f1c6d55b31e51c0375bc9864df9cb7d6e76cdfd4
albertgoncalves <goncalves.albert@gmail.com>
2018-11-25 10:59:18 -0500

    My style.

--- 99problems.ml
+++ 99problems.ml
@@ -4,35 +4,35 @@ let lst = [1; 2; 3; 4]
 
 let fac n =
     let rec loop acc = function
-    | 0 -> acc
-    | n -> (loop [@tailcall]) (acc * n) (n - 1)
+        | 0 -> acc
+        | n -> (loop [@tailcall]) (acc * n) (n - 1)
     in loop 1 n
 
 let rec last l =
     match l with
-    | [] -> None
-    | x :: [] -> Some x
-    | _ :: xs -> (last [@tailcall]) xs
+        | [] -> None
+        | x :: [] -> Some x
+        | _ :: xs -> (last [@tailcall]) xs
 
 let rec last_two l =
     match l with
-    | [] | _ :: [] -> None
-    | x :: xx :: [] -> Some [x; xx]
-    | _ :: xs -> (last_two [@tailcall]) xs
+        | [] | _ :: [] -> None
+        | x :: xx :: [] -> Some [x; xx]
+        | _ :: xs -> (last_two [@tailcall]) xs
 
 let rec at n l =
     if n < 1 then None
     else
         match (n, l) with
-        | _, [] -> None
-        | 1, (x :: _) -> Some x
-        | n, (_ :: xs) -> (at [@tailcall]) (n - 1) xs
+            | _, [] -> None
+            | 1, (x :: _) -> Some x
+            | n, (_ :: xs) -> (at [@tailcall]) (n - 1) xs
 
 let rev l =
     let rec loop l rev_l =
         match (l, rev_l) with
-        | [], rev -> rev
-        | (x :: xs), rev -> (loop [@tailcall]) xs (x :: rev)
+            | [], rev -> rev
+            | (x :: xs), rev -> (loop [@tailcall]) xs (x :: rev)
     in loop l []
 
 type 'a node =
@@ -44,9 +44,9 @@ let nest_lst =
 
 let flatten l =
     let rec loop acc = function
-    | [] -> acc
-    | One x :: xs -> (loop [@tailcall]) (x :: acc) xs
-    | Many xs :: xa -> (loop [@tailcall]) (loop acc xs) xa
+        | [] -> acc
+        | One x :: xs -> (loop [@tailcall]) (x :: acc) xs
+        | Many xs :: xa -> (loop [@tailcall]) (loop acc xs) xa
     in loop [] l
 
 let dupe_lst =
@@ -54,21 +54,21 @@ let dupe_lst =
 
 let rec compress l =
     match l with
-    | [] -> []
-    | x :: [] -> [x]
-    | x :: (xx :: xs) ->
-        if x = xx then compress (xx :: xs)
-        else x :: compress (xx :: xs)
+        | [] -> []
+        | x :: [] -> [x]
+        | x :: (xx :: xs) ->
+            if x = xx then compress (xx :: xs)
+            else x :: compress (xx :: xs)
 
 let pack l =
     let rec loop l inner outer =
         match (l, inner, outer) with
-        | [], i, o -> o @ [i]
-        | (x :: xs), [], o -> loop xs [x] o
-        | (x :: xs), (i :: is), o ->
-            let ia = (i :: is) in
-            if x = i then (loop [@tailcall]) xs (x :: ia) o
-            else (loop [@tailcall]) xs [x] (o @ [ia])
+            | [], i, o -> o @ [i]
+            | (x :: xs), [], o -> loop xs [x] o
+            | (x :: xs), (i :: is), o ->
+                let ia = (i :: is) in
+                if x = i then (loop [@tailcall]) xs (x :: ia) o
+                else (loop [@tailcall]) xs [x] (o @ [ia])
     in loop l [] []
 
 let drop l nth =
@@ -76,10 +76,10 @@ let drop l nth =
         if nth < 1 then l
         else
             match (l, n, nth) with
-            | [], _, _ -> []
-            | (x :: xs), n, nth ->
-                if n mod nth = 0 then loop xs (n + 1) nth
-                else x :: loop xs (n + 1) nth
+                | [], _, _ -> []
+                | (x :: xs), n, nth ->
+                    if n mod nth = 0 then loop xs (n + 1) nth
+                    else x :: loop xs (n + 1) nth
     in loop l 1 nth
 
 let range a b =
@@ -94,10 +94,10 @@ let is_prime m =
     if m < 2 then false
     else
         let rec loop m = function
-        | 1 -> true
-        | n ->
-            if m mod n <> 0 then loop m (n - 1)
-            else false
+            | 1 -> true
+            | n ->
+                if m mod n <> 0 then loop m (n - 1)
+                else false
         in loop m (m - 1)
 
 let rec gcd a b =
@@ -112,10 +112,10 @@ let phi m =
     else if m < 1 then 0
     else
         let rec loop m n = function
-        | 1 -> n
-        | r ->
-            if coprime m r then loop m (n + 1) (r - 1)
-            else loop m n (r - 1)
+            | 1 -> n
+            | r ->
+                if coprime m r then loop m (n + 1) (r - 1)
+                else loop m n (r - 1)
         in loop m 1 (m - 1)
 
 let factors m =
--- line.ml
+++ line.ml
@@ -5,12 +5,12 @@ Graphics.open_graph " 200x200";;
 
 let f x = x + 1 in
 for i = 0 to 200 do
-   Graphics.plot i (f i)
+    Graphics.plot i (f i)
 done
 
 let rec interactive () =
-  let event = Graphics.wait_next_event [Key_pressed] in
-  if event.key == 'q' then exit 0
-  else print_char event.key; print_newline (); interactive ()
+    let event = Graphics.wait_next_event [Key_pressed] in
+    if event.key == 'q' then exit 0
+    else print_char event.key; print_newline (); interactive ()
 
 let () = interactive ()
