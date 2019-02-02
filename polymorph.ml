f266395b61515c4a8615a9cce9a224095d758e58
albertgoncalves <goncalves.albert@gmail.com>
2019-02-02 15:22:45 -0500

    More.

--- polymorph.ml
+++ polymorph.ml
@@ -3,23 +3,32 @@ module L = List
 module type Semigroup = sig
     type t
     val to_string : t -> string
+    val to_int : t -> int
 end
 
 module Utils (S: Semigroup) = struct
-    let string_num x = S.to_string x
+    let string_num = S.to_string
+    let force_int = S.to_int
 end
 
 module IntUtils = Utils
         ( struct type t = int
-            let to_string : (t -> string) = string_of_int end
+            let to_string : (t -> string) = string_of_int
+            let to_int : (t -> int) = fun x -> x end
         )
 
 module FloatUtils = Utils
         ( struct type t = float
-            let to_string : (t -> string) = string_of_float end
+            let to_string : (t -> string) = string_of_float
+            let to_int : (t -> int) = int_of_float end
         )
 
+let (@.) (f : ('b -> 'c)) (g : ('a -> 'b)) : ('a -> 'c) = fun x -> f @@ g x
+
 let main () =
-    L.iter print_endline [IntUtils.string_num 10; FloatUtils.string_num 10.01]
+    L.iter print_endline [IntUtils.string_num 10; FloatUtils.string_num 10.01];
+    L.iter
+        (print_endline @. IntUtils.string_num)
+        [IntUtils.force_int 20; FloatUtils.force_int 20.012]
 
 let () = main ()
