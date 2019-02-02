bbcfa742baad0f26a1bac571956d9812710a7f5d
albertgoncalves <goncalves.albert@gmail.com>
2019-02-02 14:58:30 -0500

    Styles.

--- polymorph.ml
+++ polymorph.ml
@@ -9,15 +9,15 @@ module Utils (S: Semigroup) = struct
     let string_num x = S.to_string x
 end
 
-module IntUtils =
-    Utils(struct type t = int
-        let to_string : (t -> string) = string_of_int
-    end)
+module IntUtils = Utils
+        ( struct type t = int
+            let to_string : (t -> string) = string_of_int end
+        )
 
-module FloatUtils =
-    Utils(struct type t = float
-        let to_string : (t -> string) = string_of_float
-    end)
+module FloatUtils = Utils
+        ( struct type t = float
+            let to_string : (t -> string) = string_of_float end
+        )
 
 let main () =
     L.iter print_endline [IntUtils.string_num 10; FloatUtils.string_num 10.01]
