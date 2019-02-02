0c4b300133458d30e8fb35a6b2e825af3142e2f0
albertgoncalves <goncalves.albert@gmail.com>
2019-02-02 13:33:01 -0500

    Pipes.

--- re/main.ml
+++ re/main.ml
@@ -1,10 +1,12 @@
 module R = Re
 
 let main () =
-    let s : string =
-        "pNy.sQA/_\"ZHo/O:z4(6L4C,95\"I)g4d(gwZ" in
     let re : R.re =
         R.compile @@ R.Pcre.re "[/:\\\\. (),'\"]+" in
-    print_string @@ R.replace_string re ~by:"_" s
+    let s : string =
+        "pNy.sQA/_\"ZHo/O:z4(6L4C,95\"I)g4d(gwZ" in
+    s
+    |> R.replace_string re ~by:"_"
+    |> print_string
 
 let () = main ()
