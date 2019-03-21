15b5c897aac02e0414d2b39c20d9fa6874134d6f
albertgoncalves <goncalves.albert@gmail.com>
2019-03-20 21:15:07 -0400

    nice

--- shell.nix
+++ shell.nix
@@ -1,21 +1,23 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs; mkShell {
     name = "OCaml";
-    buildInputs = [ ocaml-ng.ocamlPackages_4_07.ocaml
-                    ocaml-ng.ocamlPackages_4_07.cairo2
-                    ocaml-ng.ocamlPackages_4_07.batteries
-                    ocaml-ng.ocamlPackages_4_07.cohttp-lwt-unix
-                    ocaml-ng.ocamlPackages_4_07.csv
-                    ocaml-ng.ocamlPackages_4_07.re
-                    # ocaml-ng.ocamlPackages_4_07.core
-                    ocaml-ng.ocamlPackages_4_07.findlib
-                    ocaml-ng.ocamlPackages_4_07.ocp-indent
-                    ocaml-ng.ocamlPackages_4_07.utop
-                    rlwrap
-                    tmux
-                    clang
-                    python36Packages.csvkit
-                  ];
+    buildInputs = [
+        (with ocaml-ng.ocamlPackages_4_07; [
+            ocaml
+            cairo2
+            batteries
+            cohttp-lwt-unix
+            csv
+            re
+            findlib
+            ocp-indent
+            utop
+        ])
+        rlwrap
+        tmux
+        clang
+        python36Packages.csvkit
+    ];
     shellHook = ''
         alias rocaml="rlwrap ocaml"
         echo $PATH > .nix_path
