d4166a6a5cf81add904f81017db6fb0564153b03
albertgoncalves <goncalves.albert@gmail.com>
2019-04-26 23:26:34 -0400

    clarity

--- modules.ml
+++ modules.ml
@@ -1,13 +1,13 @@
 let (|.) (f : 'b -> 'c) (g : 'a -> 'b) : ('a -> 'c) = fun x -> f (g x)
 let (>>) (a : unit -> 'a) (b : unit -> 'b) : 'b = a () |> (fun _ -> b ())
 
-module type X = sig
+module type T = sig
     type _ t
     val init : int -> (int -> 'a) -> 'a t
     val iter : ('a -> unit) -> 'a t -> unit
 end
 
-module F (X : X) = struct
+module F (X : T) = struct
     include X
     let range (a : int) (b : int) : int t =
         if a < b then
