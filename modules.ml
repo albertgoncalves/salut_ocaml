58915e241e0f906139be012fc2c17a12f7fa2dc7
albertgoncalves <goncalves.albert@gmail.com>
2019-04-26 23:07:20 -0400

    finally, modules!

--- modules.ml
+++ modules.ml
@@ -1,16 +1,13 @@
+let (|.) (f : 'b -> 'c) (g : 'a -> 'b) : ('a -> 'c) = fun x -> f (g x)
+let (>>) (a : unit -> 'a) (b : unit -> 'b) : 'b = a () |> (fun _ -> b ())
+
 module type X = sig
     type _ t
     val init : int -> (int -> 'a) -> 'a t
     val iter : ('a -> unit) -> 'a t -> unit
 end
 
-module type Y = sig
-    type _ t
-    val range : int -> int -> int t
-    val print : int t -> unit
-end
-
-module F (X : X) : (Y with type 'a t := 'a X.t) = struct
+module F (X : X) = struct
     include X
     let range (a : int) (b : int) : int t =
         if a < b then
@@ -18,26 +15,32 @@ module F (X : X) : (Y with type 'a t := 'a X.t) = struct
         else
             X.init (a - b) (fun i -> a - i)
     let print (xs : int t) : unit =
-        X.iter (fun x -> string_of_int x |> print_endline) xs
+        X.iter (print_endline |. string_of_int) xs
 end
 
-module A : (X with type 'a t = 'a array) = struct
-    type 'a t = 'a array
-    let init = Array.init
-    let iter = Array.iter
+module A = struct
+    include Array
+    include
+        F ( struct
+            type 'a t = 'a array
+            let init = Array.init
+            let iter = Array.iter end
+        )
 end
 
-module L : (X with type 'a t = 'a list) = struct
-    type 'a t = 'a list
-    let init = List.init
-    let iter = List.iter
+module L = struct
+    include List
+    include
+        F ( struct
+            type 'a t = 'a list
+            let init = List.init
+            let iter = List.iter end
+        )
 end
-
-module AF = F(A)
-module LF = F(L)
 
 let main () =
-    LF.print (LF.range 0 10)
-    ; AF.print (AF.range 10 0)
+    let l () = L.print (L.range 0 10) in
+    let a () = A.print (A.range 10 0) in
+    l >> a
 
 let () = main ()
